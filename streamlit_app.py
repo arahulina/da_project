7513fa45f6acfd65c85afc634ec26464f85ed6f7
Anna Rahulina <96630599+arahulina@users.noreply.github.com>
2024-10-08 20:47:40 +0000

    add map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title("earthquakes")
 st.write(
     "Let's start!"
 )
+
+# Завантаження даних з файлу CSV 
+df = pd.read_csv('data/earthquake_1995-2023.csv')
+
+# Відображення заголовку
+st.title("Моя карта на основі даних")
+
+# Відображення DataFrame
+#st.write("Набір даних:")
+#st.write(df)
+
+# Відображення карти на основі даних про широту і довготу
+st.map(df[['latitude', 'longitude']])
