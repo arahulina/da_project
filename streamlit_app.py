976a05a6d181756f26451b3badcbd36bada17ad0
Anna Rahulina <96630599+arahulina@users.noreply.github.com>
2024-11-11 18:30:16 +0000

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,7 @@ else:
     # Створення базової карти з фокусом на середні координати
     m = folium.Map(location=[filtered_data['latitude'].mean(), filtered_data['longitude'].mean()], zoom_start=3)
 
-    st.emprty()
+    #st.emprty()
 
     # Додавання точок землетрусів на карту
     for _, row in filtered_data.iterrows():
